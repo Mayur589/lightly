2d6f425c648b78c68d592bdd6ac7df63cfc60952
Mayur <mayurhpatel05@gmail.com>
2026-08-26 19:31:39 +0530

    Connect the backend with the local database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"lightly/internal/database"
 	"lightly/internal/handler"
+
 	"log"
 	"net/http"
 )
 
 func main() {
 	// connect database
+
+	pool, err := database.Connect()
+	if err != nil {
+		log.Fatalf("Database connection failed: %v", err)
+	}
+	defer pool.Close()
+
+	fmt.Println("Successfully connected to PostgreSQL!")
 
 	mux := http.NewServeMux()
 	handler.HandleRoutes(mux)
--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -1,1 +1,47 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func Connect() (*pgxpool.Pool, error) {
+	_ = godotenv.Load()
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL is not set")
+	}
+
+	ctx := context.Background()
+
+	config, err := pgxpool.ParseConfig(dbURL)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse DATABASE_URL: %w", err)
+	}
+
+	// Pool settings
+	config.MaxConns = 20
+	config.MinConns = 2
+	config.MaxConnLifetime = time.Hour
+	config.MaxConnIdleTime = 30 * time.Minute
+
+	pool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create connection pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, fmt.Errorf("unable to ping database: %w", err)
+	}
+
+	return pool, nil
+
+}
